a204159dbc8c84b2aa2d3ba0fc85766fd830c157
Jani <aijala.j@gmail.com>
2023-10-09 15:47:03 +0300

    added divs to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>Title of the page</h1>
+    
+      <div className="SearchBar"></div>
+      <button className="Search">Search</button>
+      <div className="SearchResults"></div>
+      <div className="Playlist"></div>
+      <div className="Tracklist"></div>
+      <div className="Track"></div>
+      <button className="SaveToSpotify">Save To Spotify</button>
+
     </div>
   );
 }
